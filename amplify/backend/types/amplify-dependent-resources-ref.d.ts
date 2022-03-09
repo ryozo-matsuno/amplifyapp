1d5b8bae43dd29a8d09603a57a60c325b4337d68
ryozo-matsuno <ryozo.matsuno@gmail.com>
2022-03-09 23:19:31 +0900

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp2b6f062f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import AddBI from './AddBI';
 import ListBI from './ListBI';
 import NoMatch from './NoMatch';
 
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+
 class App extends React.Component {
 
   constructor(props) {
@@ -14,23 +22,28 @@ class App extends React.Component {
   render() {
 
     return (
-      <BrowserRouter>
-        <h1>Book Information System</h1>
-        <div>
-        <button>
-            <Link to="/">BI追加</Link>
-          </button>
-          <button>
-            <Link to="/ListBI">BI一覧</Link>
-          </button>
-        </div>
+      <Authenticator>
+        {({signOut, user }) => (
+          <BrowserRouter>
+            <h1>Book Information System</h1>
+            <div>
+            <button>
+                <Link to="/">BI追加</Link>
+              </button>
+              <button>
+                <Link to="/ListBI">BI一覧</Link>
+              </button>
+              <button onClick={signOut}>Sign out</button>
+            </div>
 
-        <Routes>
-          <Route exact path="/" element={<AddBI />} />>
-          <Route exact path="/ListBI" element={<ListBI />} />
-          <Route exact path="*" element={<NoMatch />} />
-        </Routes>
-      </BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<AddBI />} />>
+              <Route exact path="/ListBI" element={<ListBI />} />
+              <Route exact path="*" element={<NoMatch />} />
+            </Routes>
+          </BrowserRouter>
+        )}
+      </Authenticator>
     );
   }
 }
